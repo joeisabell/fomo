05f2d2f0e34dacf09d50fa9bb60019fdc0bc7c24
Joseph Isabell <josephisabell@Josephs-Air-5.app.byu.edu>
2017-01-12 09:19:58 -0700

    Added some CSS

--- fomo/fomo/settings.py
+++ fomo/fomo/settings.py
@@ -118,8 +118,12 @@ WSGI_APPLICATION = 'fomo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fomo',
+        'USER': 'postgres',
+        'PASSWORD': 'IsabellJ7260',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
