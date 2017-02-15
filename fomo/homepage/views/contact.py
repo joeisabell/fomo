ee4b8b8c829ef025e6b2d28f236a1fb942e28b1b
Joe Isabell <joeisabell@me.com>
2017-02-15 11:30:27 -0700

    added product management forms to be able to update information about products

--- fomo/homepage/views/contact.py
+++ fomo/homepage/views/contact.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect, HttpResponseRedirect
-from django_mako_plus import view_function
 
+from django_mako_plus import view_function
 from .. import dmp_render, dmp_render_to_string
 
 @view_function
--- fomo/homepage/views/index.py
+++ fomo/homepage/views/index.py
@@ -1,6 +1,7 @@
 from django.conf import settings
-from django_mako_plus import view_function
 from datetime import datetime
+
+from django_mako_plus import view_function
 from .. import dmp_render, dmp_render_to_string
 
 @view_function
@@ -9,5 +10,4 @@ def process_request(request):
         'now' : datetime.now(),
     }
 
-    print('>>>', context)
     return dmp_render(request, 'index.html', context)
